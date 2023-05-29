e45526289547abeb869a9f0b22e1c5e5f63bf4b1
Alexis Bourieau <alexis.bourieau@gmail.com>
2023-05-29 16:47:47 +0200

    config routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :spaceships do
+    resources :journeys, only: [:create]
+  end
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :admin do
+    resources :journeys, only: [:index]
+    resources :spaceships, only: [:index]
+  end
 end
