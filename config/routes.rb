1b1e48ca92374800963379143e55af1700241f78
Alexis Bourieau <alexis.bourieau@gmail.com>
2023-05-30 17:18:52 +0200

    config devise et update routes, ajout admin controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  resources :spaceships do
+  resources :spaceships, only: [:index, :show, :new, :create] do
     resources :journeys, only: [:create]
   end
 
   namespace :admin do
     resources :journeys, only: [:index]
-    resources :spaceships, only: [:index]
+    resources :spaceships, only: [:index, :edit, :update, :destroy]
   end
 end
