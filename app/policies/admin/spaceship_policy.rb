8d51a499f204e11b04a7dcf144f47312160fec50
Alexis Bourieau <alexis.bourieau@gmail.com>
2023-05-30 12:47:36 +0200

    fix bug pundit

--- app/policies/admin/spaceship_policy.rb
+++ app/policies/admin/spaceship_policy.rb
@@ -2,19 +2,19 @@ class Admin::SpaceshipPolicy < ApplicationPolicy
   class Scope < Scope
     # NOTE: Be explicit about which records you allow access to!
     def resolve
-      scope.all
+      scope.where(user: user)
     end
+  end
 
-    def create?
-      true
-    end
+  def create?
+    true
+  end
 
-    def update?
-      record.user == user
-    end
+  def update?
+    record.user == user
+  end
 
-    def destroy?
-      record.user == user
-    end
+  def destroy?
+    record.user == user
   end
 end
