5a19ef814c9dd24c485d0f71caf58c13269d7410
Alexis Bourieau <alexis.bourieau@gmail.com>
2023-06-01 15:19:48 +0200

    front end sur index admin journey et spaceship

--- app/controllers/admin/journeys_controller.rb
+++ app/controllers/admin/journeys_controller.rb
@@ -4,4 +4,11 @@ class Admin::JourneysController < ApplicationController
     @journeys = policy_scope([:admin, Journey])
   end
 
+  def destroy
+    @journey = Journey.find(params[:id])
+    authorize [:admin, @journey]
+    @journey.destroy
+    redirect_to admin_journeys_path, status: :see_other
+  end
+
 end
